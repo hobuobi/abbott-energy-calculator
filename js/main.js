fcd04308465b161949d48e9dc96cb871eeb44251
hobuobi <obuobhum@gmail.com>
2016-07-16 21:52:05 -0700

    added basic dynamic data visualization

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+// DATA MODEL
+
+var DATA= [];
+
 function NuclearToggle(o,i,v){
     this.position = o;
     this.id = i;
     this.value = v;
     this.capacityFactor = 0.9;
+    this.type = ["LC"];
 }
 
 function ResourceSlider(min, max, i, val,type,CF){
@@ -14,10 +19,11 @@ function ResourceSlider(min, max, i, val,type,CF){
     this.capacityFactor = CF;
 }
 
-function ResourceConstant(id, val,CF){
+function ResourceConstant(id, val,CF,type){
     this.id = id;
     this.value = val;
     this.capacityFactor = CF;
+    this.type = type;
 }
 
 var peakInstalledCapacity = 43556;
@@ -28,7 +34,14 @@ var demand = 42423;
 
 //Nuclear Toggle Methods
 NuclearToggle.prototype.toggle = function() { 
-    if(this.position == true){ this.position = false; }else{this.position = true}
+    if(this.position == true){ 
+        this.position = false
+        findByKey(DATA,this.id).value = 0;
+    }
+    else{
+        this.position = true
+        findByKey(DATA,this.id).value = this.value;
+    }
 }
 NuclearToggle.prototype.setID = function(x) { this.id = x }
 NuclearToggle.prototype.setValue = function(x) { this.value = x }
@@ -37,11 +50,13 @@ NuclearToggle.prototype.getElectricityGenerated = function() {
 }
 
 //Resource Sliders Methods
-ResourceSlider.prototype.setValue = function(x) { this.value = x }
+ResourceSlider.prototype.setValue = function(x) { 
+    this.value = x 
+    findByKey(DATA,this.id).value = this.value;
+    }
 ResourceSlider.prototype.getElectricityGenerated = function() {
     return (8640*this.value*this.capacityFactor)/1000;
 }
-
 
 ResourceConstant.prototype.getElectricityGenerated = function() {
     return (8640*this.value*this.capacityFactor)/1000;
@@ -58,16 +73,38 @@ var nucToggles = {
 
 }
 var resourceSliders = {
-    "solarpv": new ResourceSlider(669,20000,"solarpv", 10000,"renewable",.15),
-    "wind": new ResourceSlider(652, 4000, "wind", 2500,"renewable",.3), 
-    "coal": new ResourceSlider(0,15297,"coal", 10000,"fossil_fuel",.81),
-    "oil": new ResourceSlider(0, 3325, "oil", 1675,"fossil_fuel",.21), 
-    "gas": new ResourceSlider(0,26090, "gas", 13045,"fossil_fuel",.53)
+    "solarpv": new ResourceSlider(669,20000,"solarpv", 10000,["R"],.15),
+    "wind": new ResourceSlider(652, 4000, "wind", 2500,["R"],.3), 
+    "coal": new ResourceSlider(0,15297,"coal", 10000,["FF"],.81),
+    "oil": new ResourceSlider(0, 3325, "oil", 1675,["FF"],.21), 
+    "gas": new ResourceSlider(0,26090, "gas", 13045,["FF"],.53)
 }
 var constants = {
-    "biogas": new ResourceConstant("biogas", 623,.65),
-    "hydrostorage": new ResourceConstant("hydrostorage",2602,1.0),
-    "hydropower": new ResourceConstant("hydropower", 2102,.4)
+    "biogas": new ResourceConstant("biogas", 623,.65,[""]),
+    "hydrostorage": new ResourceConstant("hydrostorage",2602,1.0,["LC","R"]),
+    "hydropower": new ResourceConstant("hydropower", 2102,.4,["LC", "R"]),
+    "geothermal": new ResourceConstant("geothermal", 0.4,0,["LC", "R"])
+}
+for(var item in nucToggles){
+    DATA.push({
+        "name" : nucToggles[item].id,
+        "value": 0,
+        "type": nucToggles[item].type
+    });
+}
+for(var item in resourceSliders){
+    DATA.push({
+        "name" : resourceSliders[item].id,
+        "value": resourceSliders[item].value,
+        "type": resourceSliders[item].type
+    });
+}
+for(var item in constants){
+    DATA.push({
+        "name" : constants[item].id,
+        "value": constants[item].value,
+        "type": constants[item].type
+    });
 }
 function COMP_totalInstalledCapacity(){
     var TOTAL = 0;
@@ -113,6 +150,14 @@ function truncateDecimals (num, digits) {
     return parseFloat(finalResult);
 }
 
+function findByKey(array,key){
+    for(var x in array)
+    {
+        if(array[x].name == key){ return array[x]}
+    }
+    return null;
+}
+
 function COMP_electricityGenerated(){
     var TOTAL = 0;
     for(var item in nucToggles){
@@ -134,25 +179,25 @@ function COMP_electricityGenerated(){
     
 }
 
-var COMP_cecCoal = (nucToggles["coal"].getElectricityGenerated-78713540)*.0008;
-var COMP_cecNaturalGas = (nucToggles["gas"].getElectricityGenerated-70779352)*.0008;
+var COMP_cecCoal = (resourceSliders["coal"].getElectricityGenerated()-78713540)*.0008;
+var COMP_cecNaturalGas = (resourceSliders["gas"].getElectricityGenerated()-70779352)*.0008;
 
 $(document).ready(function(){
     applyToSliders(resourceSliders);
     $("input:checkbox").change(function() {
         var id = $(this).attr("id");
         nucToggles[id].toggle();
-        console.log(nucToggles[id]);
+        updateVisualization();
     })
     
     $("input[type=range]").mousedown(function() {
         $("input[type=range]").mousemove(function(){
             var id = $(this).attr("id");
         var val = document.getElementById(id).value
-        console.log(val)
         resourceSliders[id].setValue(+val);
-        console.log(resourceSliders[id]);
         $(this).next().text(val+" MW");
+        COMP();
+        updateVisualization();
         })
         
     })
@@ -172,4 +217,43 @@ $(document).ready(function(){
             $("#"+key).attr("val", sliderObj[key].value); 
         }
     }
-});
+    
+    var width = 300,
+    height = 300,
+    radius = Math.min(width, height) / 2;
+
+    var color = d3.scale.category20();
+        
+    var arc = d3.svg.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0);
+
+    var labelArc = d3.svg.arc()
+        .outerRadius(radius - 40)
+        .innerRadius(radius - 40);
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.value; });
+    var svg = d3.select("#container-O").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+    .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+function updateVisualization(){
+    
+    console.log("UPDATING");
+    var g = svg.selectAll(".arc")
+        .data(function(){
+            console.log(pie(DATA))
+            return pie(DATA)
+        })
+        .attr("class", "arc");
+
+    g.enter().append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.value); });
+
+}
+   
+ });
